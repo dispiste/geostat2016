c58dd14486bbc8057113f0d3144692c380e6b35c
dispiste <cesar.izq@gmail.com>
2016-09-22 02:37:17 +0200

    Simplify autorun.py

--- scripts/geostatsplugincompact/autorun.py
+++ scripts/geostatsplugincompact/autorun.py
@@ -19,21 +19,11 @@ Author: Cesar Martinez Izquierdo
 """
 
 class TestProcess2(RProcess):
-    def __init__(self):
-        """
-        We'll define the script to execute and the working directory on this method.
-
-        Note that it's not mandatory to have a __init__ method, as it is
-        also possible to define rscript and wd in the defineChracteristics
-        method by using:
-        self.rscript = "/path/to/my/script.r"
-        self.wd = "/my/working/directory"
-        """
-        baseDir = os.path.dirname(os.path.abspath(__file__))
-        rscript = os.path.join(baseDir, "rscript.r")
-        RProcess.__init__(self, rscript, baseDir)
 
     def defineCharacteristics(self):
+        self.wd = os.path.dirname(os.path.abspath(__file__))
+        self.rscript = os.path.join(self.wd, "rscript.r")
+        
         # Process name
         self.setName("Test R process2")
         # Process group
@@ -55,49 +45,11 @@ class TestProcess2(RProcess):
         self.R.call("load_libraries")
         self.R.call("doalmostnothing", *args)
 
-        
-        """
-        Alternative way: directly access params by name
-
-        # this gets a gvSIG/Sextante layer object:
-        in_vector_layer = self.getParamValue("IN_VECTOR")
-        in_raster_layer = self.getParamValue("IN_RASTER")
-        # we use rlib to convert the layer to an OGR DSN and an OGR layer name
-        in_vector_dsn = rlib.getLayerPath(in_vector_layer)
-        in_layer_name = rlib.getLayerName(in_vector_layer)
-        in_raster_dsn = rlib.getLayerPath(in_raster_layer)
-
-        # for output layers, getOutputValue directly returns a path, as the layer does not exist yet
-        outRaster = self.getOutputValue("OUT_RASTER")
-        self.R.call("doalmostnothing", in_vector_dsn, in_layer_name, in_raster_dsn, outRaster)
-        """
-
-        """
-        Alternative way: individually access each parameter
-
-        in_vector_dsn = args[0]
-        in_layer_name = args[1]
-        inRaster = args[2]
-        outRaster = args[3]
-        self.R.call("doalmostnothing", in_vector_dsn, in_layer_name, inRaster, outRaster)
-        """
-
 class TestProcess3(RProcess):
-    def __init__(self):
-        """
-        We'll define the script to execute and the working directory on this method.
-
-        Note that it's not mandatory to have a __init__ method, as it is
-        also possible to define rscript and wd in the defineChracteristics
-        method by using:
-        self.rscript = "/path/to/my/script.r"
-        self.wd = "/my/working/directory"
-        """
-        baseDir = os.path.dirname(os.path.abspath(__file__))
-        rscript = os.path.join(baseDir, "rscript.r")
-        RProcess.__init__(self, rscript, baseDir)
-
     def defineCharacteristics(self):
+        self.wd = os.path.dirname(os.path.abspath(__file__))
+        self.rscript = os.path.join(self.wd, "rscript.r")
+
         # Process name
         self.setName("Test R process3")
         # Process group
@@ -113,45 +65,15 @@ class TestProcess3(RProcess):
         self.addOutputRasterLayer("OUT_RASTER", "Output raster")
         
     def callRProcess(self, *args):
-        """
-        Calls the main method of the defined R script
-        """
         self.R.call("load_libraries")
         self.R.call("doalmostnothing", *args)
 
-        
-        """
-        Alternative way: directly access params by name
-
-        # this gets a gvSIG/Sextante layer object:
-        in_vector_layer = self.getParamValue("IN_VECTOR")
-        in_raster_layer = self.getParamValue("IN_RASTER")
-        # we use rlib to convert the layer to an OGR DSN and an OGR layer name
-        in_vector_dsn = rlib.getLayerPath(in_vector_layer)
-        in_layer_name = rlib.getLayerName(in_vector_layer)
-        in_raster_dsn = rlib.getLayerPath(in_raster_layer)
-
-        # for output layers, getOutputValue directly returns a path, as the layer does not exist yet
-        outRaster = self.getOutputValue("OUT_RASTER")
-        self.R.call("doalmostnothing", in_vector_dsn, in_layer_name, in_raster_dsn, outRaster)
-        """
-
-        """
-        Alternative way: individually access each parameter
-
-        in_vector_dsn = args[0]
-        in_layer_name = args[1]
-        inRaster = args[2]
-        outRaster = args[3]
-        self.R.call("doalmostnothing", in_vector_dsn, in_layer_name, inRaster, outRaster)
-        """
 
 def main():
-    process1 = TestProcess2()
-    process1.selfregister("Scripting")
-    #process1.updateToolbox() #not necessary at startup
-    process2 = TestProcess3()
-    process2.selfregister("Scripting")
-    #process2.updateToolbox() #not necessary at startup
+    process = TestProcess2()
+    process.selfregister("Scripting")
+    process = TestProcess3()
+    process.selfregister("Scripting")
+    #process.updateToolbox() #not necessary at startup
 
 main()
